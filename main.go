2b9be423445bc72e34f23d8d24208cd120244112
Toshihiro Arae <aroearue@gmail.com>
2019-08-23 16:08:36 +0900

    Update main.go

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/rivo/tview"
 	"math/rand"
+	"reflect"
 	"time"
 )
 
@@ -15,6 +16,8 @@ func main() {
 	var (
 		RN    int
 		CN    int
+		RCN   int
+		NMAPS int
 		DELAY time.Duration
 	)
 	flag.IntVar(&RN, "r", 20, "number of rows, int")
@@ -22,61 +25,97 @@ func main() {
 	flag.DurationVar(&DELAY, "d", 50*time.Millisecond, "delay time, duration")
 	flag.Parse()
 
-	cell_map := create_newmap(RN, CN)
-
+	RCN = RN * CN
+	NMAPS = 2
 	app := tview.NewApplication()
 
-	textView := tview.NewTextView().SetChangedFunc(func() {
+	text_map := tview.NewTextView().SetChangedFunc(func() {
 		app.Draw()
 	})
 
-	textView2 := tview.NewTextView().SetChangedFunc(func() {
+	text_cycle := tview.NewTextView().SetChangedFunc(func() {
 		app.Draw()
 	})
+
+	text_list := tview.NewTextView().SetScrollable(true).
+		SetChangedFunc(func() {
+			app.Draw()
+		})
+
+	cms := NewCellmaps(RN, CN, NMAPS)
 
 	go func() {
-		pre_map := cell_map[:]
-		count := 0
+		game_count := 1
+		cycle_count := 0
+		temp_list := ""
+		pre_cm := make([]int, RCN)
+		last_cm := make([]int, RCN)
+		new_cm := make([]int, RCN)
 		for {
-			count++
+			cycle_count++
 			temp := ""
-			new_map := next_map(RN, CN, pre_map)
-			for rn := 0; rn < RN; rn++ {
-				for cn := 0; cn < CN; cn++ {
-					if new_map[rn*CN+cn] == 0 {
-						temp = temp + D
-					} else {
-						temp = temp + L
+
+			pre_cm = cms.cells[0:RCN]
+			last_cm = cms.cells[RCN:]
+			new_cm = next_map(RN, CN, pre_cm)
+
+			if reflect.DeepEqual(new_cm, last_cm) {
+				temp_list = temp_list + fmt.Sprintf("game %d: %d cycle\n", game_count, cycle_count)
+				text_list.SetText(temp_list)
+				game_count++
+				cms = NewCellmaps(RN, CN, NMAPS)
+				cycle_count = 0
+				//break
+			} else {
+				for rn := 0; rn < RN; rn++ {
+					for cn := 0; cn < CN; cn++ {
+						if new_cm[rn*CN+cn] == 0 {
+							temp = temp + D
+						} else {
+							temp = temp + L
+						}
 					}
+					temp = temp + "\n"
 				}
-				temp = temp + "\n"
+
+				cms.cells = append(new_cm, cms.cells[0:RCN]...)
+
+				text_map.SetText(temp).SetTextAlign(tview.AlignCenter)
+				text_cycle.SetText(fmt.Sprintf("cycle: %d", cycle_count)).
+					SetTextAlign(tview.AlignCenter)
+				time.Sleep(DELAY)
 			}
-			//textView.Clear()
-			//fmt.Fprintf(textView, "%s\n", temp)
-			textView.SetText(temp).SetTextAlign(tview.AlignCenter)
-			textView2.SetText(fmt.Sprintf("cycle: %d", count)).
-				SetTextAlign(tview.AlignCenter)
-			pre_map = new_map[:]
-			time.Sleep(DELAY)
 		}
 	}()
 
-	flex := tview.NewFlex().SetDirection(tview.FlexRow).
-		AddItem(textView2, 0, 1, true).
-		AddItem(textView, 0, 10, true)
+	flex := tview.NewFlex().
+		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+			AddItem(text_cycle, 0, 1, true).
+			AddItem(text_map, 0, 9, true), 0, 1, true).
+		AddItem(text_list, 0, 1, true)
 
 	if err := app.SetRoot(flex, true).Run(); err != nil {
 		panic(err)
 	}
 }
 
-func create_newmap(row_n int, col_n int) []int {
-	rand.Seed(time.Now().Unix())
-	new_cell_map := make([]int, row_n*col_n)
-	for i := 0; i < row_n*col_n; i++ {
-		new_cell_map[i] = rand.Intn(2)
+type Cellmaps struct {
+	rn    int
+	cn    int
+	n     int
+	cells []int
+}
+
+func NewCellmaps(rn, cn, n int) *Cellmaps {
+	cms := &Cellmaps{rn: rn, cn: cn, n: n}
+	cms.Initialize()
+	return cms
+}
+func (self *Cellmaps) Initialize() {
+	self.cells = make([]int, self.cn*self.rn*self.n)
+	for i := 0; i < self.cn*self.rn; i++ {
+		self.cells[i] = rand.Intn(2)
 	}
-	return new_cell_map
 }
 
 func calc_next(me int, others []int) int {
@@ -95,7 +134,7 @@ func calc_next(me int, others []int) int {
 }
 
 func next_map(row_n int, col_n int, mapA []int) []int {
-	map_len := len(mapA)
+	map_len := col_n * row_n
 	mapB := make([]int, map_len)
 	for i := 0; i < row_n; i++ {
 		for j := 0; j < col_n; j++ {
